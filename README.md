58a494aef2a7cc4c226d7f15a2164b459f41aaba
John Syrinek <john.syrinek@gmail.com>
2023-02-05 22:28:51 -0600

    more

---
+++ README.md
@@ -0,0 +1,16 @@
+# Astrophotography image processing
+
+## Curriculum
+
+1. Introduction to astrophotography: Learn the basics of astrophotography, including the equipment used, camera settings, and exposure times.
+2. Image acquisition: Learn how to capture images of the night sky using a digital camera or a telescope.
+3. Image calibration: Learn how to process raw astronomical images, including dark frame subtraction, flat field correction, and bias correction.
+4. Image stacking: Learn how to combine multiple images to increase the signal-to-noise ratio and produce a final image with greater detail.
+   1. Registration: Learn how to align and register multiple images to be stacked, including techniques for eliminating field rotation, field curvature, and focal plane distortion.
+   2. Methods: Learn about different image stacking methods, including average stacking, median stacking, and sigma clipping.
+   3. Settings: Learn how to adjust image stacking settings to produce the best final image, including choosing the right number of images to stack and the best stacking method.
+   4. Post-processing: Learn how to post-process the final stacked image using techniques such as histogram stretching, noise reduction, and sharpening.
+5. Image enhancement: Learn how to process and enhance final images using techniques such as histogram stretching, noise reduction, and sharpening.
+6. Color processing: Learn how to create a color image from a set of monochrome images captured through different filters.
+7. Advanced techniques: Learn about advanced techniques such as luminance layering, layer masks, and star tracking for capturing sharp images of the night sky.
+8. Image analysis: Learn how to analyze astrophotographic images to extract scientific information, such as determining the position and brightness of stars, the size of galaxies, and the presence of nebulae.
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import math
 import os
 
 import cv2
 import matplotlib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import skimage
+
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
-import numpy as np
 import rawpy
+from parallel_pandas import ParallelPandas
 
-
+ParallelPandas.initialize(n_cpu=12)
 
 def info(path):
     print(path)
@@ -20,40 +26,74 @@ def info(path):
             print(raw.postprocess())
 
 
+def read_raw(path):
+    return rawpy.imread(path).postprocess()
+
+
 def stack(path):
     M = None
     first_image = None
     last_image = None
-    files = 0
-    for file in os.listdir(path):
-        files += 1
-        print(f'Processing {path}{file}')
-        with rawpy.imread(f'{path}{file}') as raw:
-            image = cv2.cvtColor(raw.postprocess(), cv2.COLOR_RGB2BGR).astype(np.float32) / 255
-            last_image = image
-            if first_image is None:
-                # convert to gray scale floating point image
-                first_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                stacked_image = image
-            else:
-                # Estimate perspective transform
-                s, M = cv2.findTransformECC(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), first_image, M,
-                                            cv2.MOTION_HOMOGRAPHY)
-                w, h, _ = image.shape
-                # Align image to first image
-                image = cv2.warpPerspective(image, M, (h, w))
-                stacked_image += image
-    stacked_image /= files
-    stacked_image = (stacked_image * 255).astype(np.uint8)
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.set_title('last')
-    ax1.imshow(last_image)
-    ax2.set_title('stacked')
-    ax2.imshow(stacked_image)
+    files = pd.DataFrame([f'{path}{file}' for file in os.listdir(path)], columns=['path'])
+    files['raw'] = files['path'].apply(read_raw)
+    raw = files['raw']
+    show_hist(raw[0])
+    show_subs(files)
     plt.show()
-    cv2.imwrite('output.jpg', stacked_image)
-    cv2.waitKey(0)
+    #    image = cv2.cvtColor(raw.postprocess(), cv2.COLOR_RGB2BGR).astype(np.float32) / 255
+    #    last_image = image
+    #    if first_image is None:
+    #        # convert to gray scale floating point image
+    #        first_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #        stacked_image = image
+    #    else:
+    #        # Estimate perspective transform
+    #        s, M = cv2.findTransformECC(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), first_image, M,
+    #                                    cv2.MOTION_HOMOGRAPHY)
+    #        w, h, _ = image.shape
+    #        # Align image to first image
+    #        image = cv2.warpPerspective(image, M, (h, w))
+    #        stacked_image += image
+    # stacked_image /= len(files)
+    # stacked_image = (stacked_image * 255).astype(np.uint8)
+    # fig, (ax1, ax2) = plt.subplots(1, 2)
+    # ax1.set_title('last')
+    # ax1.imshow(last_image)
+    # ax2.set_title('stacked')
+    # ax2.imshow(stacked_image)
+    # plt.show()
+    # cv2.imwrite('output.jpg', stacked_image)
+    # cv2.waitKey(0)
+
+
+def show_subs(files):
+    rows = cols = math.ceil(math.sqrt(files.shape[0]))
+    fig, axs = plt.subplots(rows, cols, squeeze=False)
+    axs = axs.flatten()
+    for (i, file) in enumerate(files['raw']):
+        axs[i].imshow(cv2.cvtColor(file, cv2.COLOR_BGR2RGB))
+        axs[i].axis('off')
+    plt.tight_layout()
+    plt.subplots_adjust(top=1,
+                        bottom=0,
+                        left=0,
+                        right=1,
+                        hspace=0,
+                        wspace=0)
+
+def show_hist(file):
+    fig, axs = plt.subplots(3, sharex=True, sharey=True)
+    for (i, color) in enumerate(['red', 'green', 'blue']):
+        hist, bins = skimage.exposure.histogram(file[..., i], source_range='dtype')
+        axs[i].plot(bins, hist)
+        axs[i].set_title(color)
+
+
+
+def apply_theme():
+    plt.tight_layout()
 
 
 if __name__ == '__main__':
+    apply_theme()
     stack('input/M42 RAW 41F ISO800 8min/')
